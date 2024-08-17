cfde505bc7168f581eb03ca06eef8a51abdae054
your name <your email>
2024-08-17 11:34:48 +0800

    图片

---
+++ src/pages/pc/banner/index.tsx
@@ -0,0 +1,13 @@
+ import style from "@/styles/pc/banner.module.scss"
+ import Image from 'next/image'
+
+ const Banner = () => {
+    return (
+        <div className={style.banner}>
+            轮播图123
+           <img className={style.bannerImg} src="/imgs/0.webp" alt="me" />
+        </div>
+    );
+}
+
+ export default Banner
--- src/pages/pc/index.tsx
+++ src/pages/pc/index.tsx
@@ -1,10 +1,13 @@
  import Header from "./header"
  import Nav from "./nav"
+ import Banner from "./banner";
  const Index = () => {
     return (
         <div>
             <Header></Header>
             <Nav></Nav>
+            <Banner></Banner>
+            
             pcpcpcpcpcpcpcpc
         </div>
     );
