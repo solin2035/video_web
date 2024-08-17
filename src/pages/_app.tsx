0a7e72a0c9a9e4f851fcf599a075f68ef8cfe11b
your name <your email>
2024-08-17 13:42:25 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,22 +20,24 @@ function redirect(ctx: NextPageContext, location: string) {
 MyApp.getInitialProps = async (context: AppContext) => {
   const { ctx } = context;
   // 是否为移动端（平板电脑不算做移动端）
+
   const isMobileDevice = isMobile({ ua: ctx.req, tablet: false });
   // 是否为平板电脑终端
   const isTabletDevice =
-      !isMobileDevice && isMobile({ ua: ctx.req, tablet: true });
+    !isMobileDevice && isMobile({ ua: ctx.req, tablet: true });
+  console.log(`跳转路由的地址`, ctx.pathname);
   if (isMobileDevice && !ctx.pathname.startsWith("/mobile")) {
     redirect(ctx, "/mobile");
   } else if (isTabletDevice && !ctx.pathname.startsWith("/tablet")) {
     redirect(ctx, "/tablet");
   } else if (
-      !isMobileDevice &&
-      !isTabletDevice &&
-      !ctx.pathname.startsWith("/pc")
+    !isMobileDevice &&
+    !isTabletDevice &&
+    !ctx.pathname.startsWith("/pc")
   ) {
     redirect(ctx, "/pc");
   }
   return {
-    ...await App.getInitialProps(context),// 必须加上这个，表示透传_app.tsx默认的值
+    ...(await App.getInitialProps(context)), // 必须加上这个，表示透传_app.tsx默认的值
   };
 };
