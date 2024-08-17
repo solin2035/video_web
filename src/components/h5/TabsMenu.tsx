548df8f350c029c91f245c7cb679b576d059d6e0
solin <solin2035@gmail.com>
2024-08-17 14:10:12 +0800

    路由class

--- src/components/h5/TabsMenu.tsx
+++ src/components/h5/TabsMenu.tsx
@@ -2,14 +2,14 @@ import tabsMenuStyle from "@/assets/styles/h5/tabsMenu.module.scss"
 import useTabsMenu from "@/components/hook/useTabsMenu";
 
 const TabsMenu = () => {
-    const {tabs, goToPath} = useTabsMenu()
+    const {tabs, goToPath, isActive} = useTabsMenu()
 
 
     return (
         <div className={tabsMenuStyle.tabsMenu}>
             {
                 tabs.map((tab) => <div className={tabsMenuStyle.tabsMenu__item} key={tab.id}>
-                <div onClick={() => goToPath(tab.path)}>{tab.name}</div>
+                <div className={isActive && isActive(tab.path) ? tabsMenuStyle.tabsMenu__item_active : ""} onClick={() => goToPath(tab.path)}>{tab.name}</div>
             </div>)
             }
         </div>
--- src/pages/mobile/dy/index.tsx
+++ src/pages/mobile/dy/index.tsx
@@ -1,8 +1,10 @@
 import Layout from "@/components/h5/Layout";
+import Banner from "@/components/h5/Banner";
 
 const Index = () => {
     return (
         <Layout>
+            <Banner />
             电影
         </Layout>
     );
