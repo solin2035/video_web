d37a691beebb00a41a20d425a6e4c06af55a86d6
your name <your email>
2024-08-18 20:18:25 +0800

    添加标题

--- src/pages/mobile/index.tsx
+++ src/pages/mobile/index.tsx
@@ -1,13 +1,22 @@
 import Banner from "@/components/h5/Banner";
 import VideoList from "@/components/h5/VideoList/index";
 import Layout from "@/components/h5/Layout";
+import Head from "next/head";
 
 const Index = () => {
   return (
-    <Layout>
-      <Banner />
-      <VideoList></VideoList>
-    </Layout>
+    <>
+      <Head>
+        <title>最新电影尽在最新影院</title>
+        <meta property="og:title" content="最新电影尽在最新影院" key="title" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/png" href="/imgs/logo-icon.png"></link>
+      </Head>
+      <Layout>
+        <Banner />
+        <VideoList></VideoList>
+      </Layout>
+    </>
   );
 };
 
