ba2989010e76a29a9035053dc2e55091418560dd
your name <your email>
2024-08-18 13:14:37 +0800

    轮播图

--- src/components/h5/Banner.tsx
+++ src/components/h5/Banner.tsx
@@ -1,16 +1,64 @@
-import Image from 'next/image';
-import BannerStyle from "@/assets/styles/h5/banner.module.scss"
+import Image from "next/image";
+import BannerStyle from "@/assets/styles/h5/banner.module.scss";
+import { Carousel } from "antd";
+import { useMemo, useState } from "react";
 const Banner = () => {
-    return (
+  const [index, setIndex] = useState(0);
+  const [list, setList] = useState([
+    {
+      src: require("@/assets/images/h5/1.webp"),
+      intro: `巅峰3｜林更新“人菜瘾大”直呼不服1`,
+    },
+    {
+      src: require("@/assets/images/h5/1.webp"),
+      intro: `巅峰3｜林更新“人菜瘾大”直呼不服2`,
+    },
+    {
+      src: require("@/assets/images/h5/1.webp"),
+      intro: `巅峰3｜林更新“人菜瘾大”直呼不服3`,
+    },
+  ]);
+  const onChange = (currentSlide: number) => {
+    console.log(`轮播图更变`, currentSlide);
+    setIndex(currentSlide);
+  };
+  const introText = useMemo(() => {
+    return list?.[index]?.intro;
+  }, [index]);
+  return (
+    <div className={BannerStyle.wrap}>
+      <Carousel autoplay={true} afterChange={onChange}>
         <div className={BannerStyle.banner}>
-            <Image
-                className={BannerStyle.img}
-                src={require('@/assets/images/h5/1.webp')}
-                alt=""
-                priority
-            />
+          <Image
+            className={BannerStyle.img}
+            src={require("@/assets/images/h5/1.webp")}
+            alt=""
+            priority
+          />
         </div>
-    );
-}
+        <div className={BannerStyle.banner}>
+          <Image
+            className={BannerStyle.img}
+            src={require("@/assets/images/h5/1.webp")}
+            alt=""
+            priority
+          />
+        </div>
+        <div className={BannerStyle.banner}>
+          <Image
+            className={BannerStyle.img}
+            src={require("@/assets/images/h5/1.webp")}
+            alt=""
+            priority
+          />
+        </div>
+      </Carousel>
+      <h5 className={BannerStyle.intro}>
+        {introText}
+        <span>{`${index + 1} / ${list.length}`}</span>
+      </h5>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
